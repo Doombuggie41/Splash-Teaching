e5a96b1ca46844c60d4e4ed234e961631eab2ca6
Ryan Jerue <ryan@jerue.org>
2015-11-21 14:03:31 -0500

    After Class
    Better readability and comments

---
+++ Object Oriented Programming with Java 2015/Introduction/src/monster.java
@@ -0,0 +1,13 @@
+
+public class monster {
+	int health;
+	String name;
+	int attack;
+	
+	public monster(int health, String name, int attack){
+		this.health = health;
+		this.name = name;
+		this.attack = attack;
+	}
+	
+}
